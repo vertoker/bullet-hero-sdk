401d89f8b73adeb7fadc2c9825d5f45e113d7259
vertoker <vertogpro@gmail.com>
2026-09-03 03:28:10 +0500

    fix burst compatibility exception

--- Utils/GuidHelper.cs
+++ Utils/GuidHelper.cs
@@ -8,12 +8,8 @@ namespace BH.SDK.Utils
         /// <summary>
         /// Creates a Guid with the given int placed in the last 4 bytes. The first 12 bytes are zero.
         /// </summary>
-        public static Guid FromIntAtEnd(int value)
-        {
-            Span<byte> bytes = stackalloc byte[16]; // all zeros by default
-            BinaryPrimitives.WriteInt32BigEndian(bytes.Slice(12, 4), value); // big-endian write
-            return new Guid(bytes);
-        }
+        public static Guid FromIntAtEnd(int value) => new(0, 0, 0,0, 0, 0, 0,
+                (byte)(value >> 24), (byte)(value >> 16), (byte)(value >> 8), (byte)value);
         
         /// <summary>
         /// Extracts the int back from the last 4 bytes of the Guid.
