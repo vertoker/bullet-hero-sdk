e3c6c9466b13c66aba1086d485476fd73baf2a7b
vertoker <vertogpro@gmail.com>
2026-09-09 21:40:05 +0500

    update version

--- SdkVersion.cs
+++ SdkVersion.cs
@@ -23,6 +23,6 @@ namespace BH.SDK
     public static class SdkVersion
     {
         /// <summary> Semver over the public API. Read, never inlined - see this file's header. </summary>
-        public static readonly string Value = "0.6.1";
+        public static readonly string Value = "0.6.2";
     }
 }
